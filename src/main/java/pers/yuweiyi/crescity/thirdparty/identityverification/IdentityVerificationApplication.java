9ba9658a28a4c478f71607c0b1c4d516852c71d0
WaYease <yu_weiyi@outlook.com>
2024-03-08 16:50:46 +0800

    Init

---
+++ src/main/java/pers/yuweiyi/crescity/thirdparty/identityverification/IdentityVerificationApplication.java
@@ -0,0 +1,16 @@
+package pers.yuweiyi.crescity.thirdparty.identityverification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class IdentityVerificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityVerificationApplication.class, args);
+        log.info("Service started.");
+    }
+
+}
---
+++ src/test/java/pers/yuweiyi/crescity/thirdparty/identityverification/IdentityVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.yuweiyi.crescity.thirdparty.identityverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityVerificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
