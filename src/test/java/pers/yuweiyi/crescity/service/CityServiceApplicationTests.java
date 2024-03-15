e2d5bf45a2d81bb136d4de20a2031d98861fc88b
WaYease <yu_weiyi@outlook.com>
2024-03-15 14:13:31 +0800

    version 0.0.1 - 初始化。

---
+++ src/test/java/pers/yuweiyi/crescity/service/CityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.yuweiyi.crescity.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
